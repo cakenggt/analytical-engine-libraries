6e80635867c27febeb10e054b3934ecc86cc9150
Alec Lownes <alownes@wescodist.com>
2017-05-26 10:34:18 -0400

    Rewrite get to be easier for human attendant to understand

--- generate-get.js
+++ generate-get.js
@@ -1,40 +1,61 @@
 var fs = require('fs');
 
-var start = `. This library gets the value at a specified store index
+const start = 3;
+
+var total = `. This library gets the value at a specified store index
 . and stores it at V000.
 . Do not get anything below V003 with this script
 
 . v0 is store index to get from
 N001 1
-* . move index to V2
+. v2 is counter
+N002 ${start-1}
+. check for index too low and halt if true
+-
+L002
 L000
+{?
+H Cannot get store indexes lower than ${start}
+}{
++ . reset run-up lever
 L001
-S002
-- . decrement index
+L001
+}
+`;
+
+for (let i = start; i < 1000; i++) {
+	total += `
+{?
+. these blocks will run until it reaches the index it wants
++ . increment counter
 L002
 L001
-S002`;
-
-var end = '';
-
-for (let i = 1; i < 1000; i++) {
-	start += `
+S002
+. subtract index from counter to see if negative
+-
+L002
+L000
+. if this was positive, you have found your get index
 {?
-* . store value at index ${i} in V0
+. migrate this index into v0
+*
 L${i}
 L001
 S000
-- . decrement index
-L002
+. do a negative calculation to set run up lever
+-
 L001
-S002`;
-
-	end += `
+L002
+}{
+. reset the run-up lever if this wasn't your index
++
+L001
+L002
+}
 }`;
 }
 
-end += `
-. something to keep the chain from going off the end
-N001 0`;
+total += `
+. comment to keep the chain from running off the engine`;
 
-fs.writeFileSync('get.ae', start+end);
+fs.writeFileSync('get.ae', total);
