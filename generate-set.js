56a5c29a497fcfc66627f0d6520afca85b32c297
Alec Lownes <alownes@wescodist.com>
2017-05-26 10:49:11 -0400

    Modify set to use logic that a human attendant would understand

--- generate-set.js
+++ generate-set.js
@@ -1,31 +1,60 @@
 var fs = require('fs');
 
-var start = `. This library sets the value at a specified store index
+const start = 4;
+
+var total = `. This library sets the value at a specified store index
 . with a specified value.
-. Do not set anything below V003 with this script
+. Do not set anything below V004 with this script
 . v0 is store index to set
 . v1 is value to set at that index
-N002 1`;
-
-var end = `
-. something to keep the chain from going off the end
-N002 1`;
-
-for (let i = 0; i < 1000; i++) {
-	start += `
-- . decrement index
+N002 1
+. v3 is counter
+N003 ${start-1}
+. check for index too low and halt if true
+-
+L003
 L000
-L002
-S000
-{?`;
-
-	end = `
+{?
+H Cannot set store indexes lower than ${start}
 }{
-*
++ . reset run-up lever
 L002
+L002
+}`;
+
+for (let i = start; i < 1000; i++) {
+	total += `
+{?
+. these blocks will run until it reaches the index it wants
++ . increment counter
+L003
+L002
+S003
+. subtract index from counter to see if negative
+-
+L003
+L000
+. if this was positive, you have found your set index
+{?
+. set indicated index with indicated value
+*
 L001
+L002
 S${i}
-}` + end;
+. do a negative calculation to set run up lever
+-
+L002
+L003
+}{
+. reset the run-up lever if this wasn't your index
++
+L002
+L003
+}
+}`;
 }
 
-fs.writeFileSync('set.ae', start+end);
+total += `
+. comment to keep the chain from running off the engine`;
+
+fs.writeFileSync('set.ae', total);
