615bce190b26fecaa7c363cced478d398f6f10c4
Alec Lownes <alownes@wescodist.com>
2017-05-30 10:43:21 -0400

    Fix printstr and add test

--- scripts/generate-printstr.js
+++ scripts/generate-printstr.js
@@ -4,11 +4,18 @@ var path = require('path');
 const start = 30;
 
 var total = `. This library writes a character in annotation mode represented by the char code provided
+. This library only prints characters with codes greater than or equal to 30 (control codes are absent).
+. This library uses slot 0 for input.
+. This library uses slots 1 and 2 for variables.
 
 . v0 is the char code to write
 N001 1
 . v2 is the counter
 N002 ${start-1}
+. Make sure the run-up lever is reset
++
+L001
+L001
 `;
 
 for (let i = start; i < 128; i++) {
